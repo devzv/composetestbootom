4f0919512df8098051f8494d68698087dfede887
devzv <devzv@ya.ru>
2022-08-22 18:28:27 +0300

    добавил R8, lottie и выезжающий элемент

--- app/src/main/java/com/devzv/composetestbootom/screen/main/MainScreen.kt
+++ app/src/main/java/com/devzv/composetestbootom/screen/main/MainScreen.kt
@@ -1,9 +1,69 @@
 package com.devzv.composetestbootom.screen.main
 
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlin.math.min
 
 @Composable
 fun MainScreen() {
-    Text(text = "Main screen")
+    Box {
+        val scrollState = rememberScrollState()
+        Column(modifier = Modifier.verticalScroll(state = scrollState)) {
+            (1..100).forEach {
+                Text(text = "Main screen text: $it")
+            }
+        }
+
+        if (scrollState.value > 0) {
+            var cardHeight by remember { mutableStateOf(0) }
+            var cardOffset by remember { mutableStateOf(0.dp) }
+            val density = LocalDensity.current
+
+            var modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+
+            if (cardHeight == 0) {
+                modifier = modifier
+                    .alpha(0F)
+                    .onGloballyPositioned { layoutCoordinates ->
+                        cardHeight = layoutCoordinates.size.height
+                    }
+            }
+            with(density) {
+                cardOffset = (cardHeight - min(cardHeight, scrollState.value)).toDp()
+            }
+
+            Log.d("MainScreen", "cardHeight: $cardHeight / cardOffset: $cardOffset")
+
+            Card(
+                backgroundColor = Color.Green,
+                modifier = modifier.offset(y = -cardOffset)
+            ) {
+                Column {
+                    Text(text = "Card title")
+                    Text(text = "Card text")
+                }
+
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
--- app/src/main/java/com/devzv/composetestbootom/screen/shops/ShopsScreen.kt
+++ app/src/main/java/com/devzv/composetestbootom/screen/shops/ShopsScreen.kt
@@ -1,9 +1,21 @@
 package com.devzv.composetestbootom.screen.shops
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.devzv.composetestbootom.R
 
 @Composable
 fun ShopsScreen() {
-    Text(text = "Shops screen")
+    Column {
+        Text(text = "Shops screen")
+        val lottieComposition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.lottie_loading))
+        LottieAnimation(composition = lottieComposition, iterations = LottieConstants.IterateForever)
+    }
+
 }
